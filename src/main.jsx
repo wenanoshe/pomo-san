92485cdb9df1a5b4a2fa14ae94c677052be45d9a
Braider <braiderandreyp@gmail.com>
2026-06-22 22:11:48 -0500

    feat: set global Lucide strokeWidth to 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { LucideProvider } from "lucide-react";
 import Pomodoro from "./components/Pomodoro";
 import "./styles/global.scss";
 import "normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Pomodoro />
+    <LucideProvider strokeWidth={2.3}>
+      <Pomodoro />
+    </LucideProvider>
   </React.StrictMode>
 );
